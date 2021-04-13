fdd4d8c834b1efa21af2d04d07ae6efb875ba2dc
DA7IDS <davit.sulukhia@gmail.com>
2021-04-13 17:18:35 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1
+let cars = ["audi", "bmw", "lexus", "volkswagen", "porsche"];
+
+
+for (let i = 1; i < 5; i++) {
+    console.log(cars[i]);
+}
+// 2
+let names = ["gio", "nika", "mari", "nia", "mate"]
+
+for (let z = 0; z < names.length; z++) {
+    if (names[z].length > 3)
+        console.log(names[z]);
+}
+// 3
+let numbers = [7, 9, 8, 15, 27, 13, 4, 6, 12]
+
+for (let n = 0; n < numbers.length; n++) {
+    if (numbers[n] % 2 == 1)
+        console.log(numbers[n])
+}
+for (let n = 0; n < numbers.length; n++) {
+    if (numbers[n] % 2 == 0)
+        console.log(numbers[n])
+}
+// 4
+let car = ["audi", "bmw", "lexus", "volkswagen", " porsche"];
+
+console.log(car[1]);
+// 5
+var person = {
+    firstname: "john",
+    lastname: "doe",
+    age: 50,
+    eyecolor: "blue"
+};
+console.log(person.eyecolor);
+// 6
+var every = [7, 8, 9, 10]
+
+console.log(every[0])
+console.log(every[1])
+console.log(every[2])
+console.log(every[3])
+ 
